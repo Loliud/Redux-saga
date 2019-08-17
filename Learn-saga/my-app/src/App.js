0783cbc6ea02f762cdb9bf9acec26de6c392392b
Loliud <anhhaiquynhon123@gmail.com>
2019-08-17 16:26:01 +0700

    complete template

--- Learn-saga/my-app/src/App.js
+++ Learn-saga/my-app/src/App.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import './App.css';
 import ListItem from './components/ListItem/ListItem';
+import TaskForm from './components/TaskForm/TaskForm';
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      statusTaskForm: true
+    }
+  }
+
+  toggleTaskForm = () =>{
+    this.setState({
+      statusTaskForm: !this.state.statusTaskForm
+    });
+    
+  }
+
+  onToggle = (param) =>{
+    this.setState({
+      statusTaskForm: param
+    });
+  }
 
 
   render() {
+    const {statusTaskForm} = this.state;
     return (
       <div className="App" >
-        <Button className="add" color="secondary"><span>+</span> Thêm công việc</Button>
+        <Button className="add" color="secondary" onClick={this.toggleTaskForm}><span>+</span> Thêm công việc</Button>
         <ListItem />
+        <TaskForm statusTaskForm={statusTaskForm} onReceiveStatus={this.onToggle}/>
 
       </div>  
     );
---
+++ Learn-saga/my-app/src/components/TaskForm/TaskForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './style.css';
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class TaskForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: true
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+    componentWillReceiveProps() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+
+        this.props.onReceiveStatus(this.state.modal);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>Todos</ModalHeader>
+                    <ModalBody>
+                        <form id="form">
+                            <div className="group-input">
+                                <label>Title</label><input></input>
+                            </div>
+                            <div className="group-input">
+                                <label>Description</label><input></input>
+                            </div>
+
+                        </form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" onClick={this.toggle}>Save</Button>{' '}
+                        <Button color="danger" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default TaskForm;
